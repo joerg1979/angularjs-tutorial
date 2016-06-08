0c169783dbbb406a469e87684c363f7d7304ac78
JörgSundmacher <joerg.sundmacher@edicos.de>
2016-06-09 00:58:02 +0200

    added: <body ng-app="app"> for auto startup/init of app.module("app",[]). removing: <body class=ng-scope ng-app=app> via app.run()-Method reinitialize app: via manual app.bootstrap(document['app'])-call

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -1,12 +1,51 @@
 "use strict";
+// create app.module
+var app = angular.module('app', ['app.users']);
+// init interval-variable
+var intervalID;
 
-var app = angular.module('app',['app.users']);
+var con = 1;
+var conin = 1;
+var rn = 1;
+var rnin = 1;
+// 1. Running first when script is included
 
-//Running on init this module
-app.run( function () {
-    console.log("app.run() initialized");
+app.config(function () {
+    console.info("app.config() started the " + con + ". time");
+    con++;
+    if ($("body").attr('ng-app') === "app") {
+        console.info("app.config() started inside IF the " + conin + ". time");
+        conin++;
+        console.info("app.config() finished "+ (conin-1) +".time");
+    }
 });
-// Running first when script is included
-app.config( function () {
-   console.error("app.config() finished conifguration task"); 
+//Running on init this module
+
+app.run(function () {
+    console.log("app.run() started the " + rn + ". time");
+    rn++;
+    if ($("body").attr('ng-app') === "app") {
+        console.log("app.run() started the insideIF the " + rnin + ". time (body has ng-app='app')");
+        rnin++;
+        console.log("app.run() insideIF will try to removeAttr('ng-app') & .removeClass('ng-scope')");
+        $("body").removeAttr('ng-app');
+        $("body").removeClass("ng-scope");
+        alert("BODY.hasClass('ng-scope')? : "+ $('body').hasClass("ng-scope"));
+        
+        console.log("app.ready() sets trigger on doc.readyState to run angular.bootstrap()");
+// } else{
+        intervalID = setInterval(function () {
+            if (/loaded|complete/i.test(document.readyState)) {
+                console.info("angular.bootstrap() starting");
+                angular.bootstrap(document, ['app']);
+                clearInterval(intervalID);
+                if ($('body').hasClass("ng-scope")) {
+                    console.info("angular.app has been started again, now via bootstrap() starting");
+                }
+            }
+        }, 10);
+    } else {
+        
+        console.info("angular.app starting again via bootstrap() FINISHED!!!");
+    }
 });
