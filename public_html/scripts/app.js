75ee75a39cac2eec879fbc2921f6e48ee19e48ba
JörgSundmacher <joerg.sundmacher@edicos.de>
2016-06-08 22:42:03 +0200

    created: first module with .conifg() & .run()

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+"use strict";
 
+var app = angular.module('app',['app.users']);
 
+//Running on init this module
+app.run( function () {
+    console.log("app.run initialized");
+});
+// Running first when script is included
+app.config( function () {
+   console.error("app.config finished conifguration task"); 
+});
