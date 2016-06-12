76edec30093b8a83f9dabf2c725812f2e160285a
JörgSundmacher <joerg.sundmacher@edicos.de>
2016-06-12 16:32:24 +0200

    added: providerModule

---
+++ public_html/scripts/provide/provide-module.js
@@ -0,0 +1,12 @@
+var app = angular.module('app',['sdfsdf']);
+    
+app.config( function ( sdfsd ){
+    app.value('key','value');
+    sdfsd.value('prokey','pro-value');
+            
+});
+
+app.run( function ( key, prokey){
+
+});
+
